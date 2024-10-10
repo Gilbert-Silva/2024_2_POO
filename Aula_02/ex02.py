68127f24e14a631b7e52f5c20fd8cfe57d989f0e
Gilbert-Silva <gilbertif1995@outlook.com>
2024-10-10 07:43:35 -0300

    Tipos do Python

---
+++ Aula_02/ex02.py
@@ -0,0 +1,15 @@
+x = 5
+y = 5.5
+z = False
+s = "TADS - Tecnologia em Análise e Desenvolvimento de Sistemas"
+l = s.split()
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(s, type(s))
+print(l)
+print(type(l))
+l.sort()
+print(l)
+print(type(print))
+
