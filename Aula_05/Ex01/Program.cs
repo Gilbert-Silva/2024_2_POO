3a85c36f7d10d1ba928f564e00a4706b88f9b6b7
Gilbert-Silva <gilbertif1995@outlook.com>
2024-10-24 08:24:41 -0300

    Encapsulamento

---
+++ Aula_05/Ex01/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangulo x = new Triangulo();
+        Console.WriteLine("Informe o valor da base do triângulo: ");
+        x.set_base(double.Parse(Console.ReadLine()));
+        Console.WriteLine("Informe o valor da altura do triângulo: ");
+        x.set_altura(double.Parse(Console.ReadLine()));
+        Console.WriteLine($"Base = {x.get_base()}, Altura = {x.get_altura()}");
+        Console.WriteLine($"Área = {x.calc_area()}");
+        Console.ReadKey();
+    }
+}
+
+class Triangulo {
+    private double b, h;
+    public Triangulo() {
+        this.b = 0;
+        this.h = 0;
+    }    
+    public void set_base(double v) {
+        if (v > 0) b = v; 
+        else throw new ArgumentOutOfRangeException("a base do triângulo não pode ser negativa");
+    }    
+    public void set_altura(double v) {
+        if (v > 0) h = v; 
+        else throw new ArgumentOutOfRangeException("a altura do triângulo não pode ser negativa");
+    }
+    public double get_base() {
+        return b;
+    }    
+    public double get_altura() {
+        return h;
+    }    
+    public double calc_area() {
+       return b * h / 2; 
+    }        
+}
