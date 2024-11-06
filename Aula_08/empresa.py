40fac5ebdb69eb73eb9ac7786fb000bd78e7403f
Gilbert-Silva <gilbertif1995@outlook.com>
2024-11-06 13:27:46 +0000

    Sociedade

--- Aula_08/empresa.py
+++ Aula_08/empresa.py
@@ -20,6 +20,19 @@ class Cliente:
         if cpf == "": raise ValueError("Cpf inválido")
         if limite < 0: raise ValueError("Limite inválido")
     def set_socio(self, c):
+        # self é um cliente, c é um cliente
+        if self == c: raise ValueError("Não pode ser sócio dele mesmo")
+
+        # x é sócio atual de self: pode ser None ou não
+        x = self.__socio
+        # x deixa de ter como sócio self
+        if x != None: x.__socio = None
+
+        # y é o sócio atual de c: pode ser None ou não
+        y = c.__socio
+        # y deixa de ter como sócio c
+        if y != None: y.__socio = None
+
         self.__socio = c
         c.__socio = self
     def get_limite(self):
